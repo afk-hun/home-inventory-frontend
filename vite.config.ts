46f82b8ef961ae250c446b42b11b05f51105d24e
AFK (Akos Ferenc Kalamar) <akos.kalamar@gmail.com>
2026-02-20 13:10:43 +0100

    build: add docker file and config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'node:path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	preview: {
+		port: 5173,
+		strictPort: true,
+	},
+	server: {
+		port: 5173,
+		strictPort: true,
+		host: true,
+		origin: "http://0.0.0.0:5173",
+	},
 });

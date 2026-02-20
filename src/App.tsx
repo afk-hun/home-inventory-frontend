1accec71a7f41776470ec080cfbda62fbc5b817f
AFK (Akos Ferenc Kalamar) <akos.kalamar@gmail.com>
2026-02-20 11:17:59 +0100

    feat: init vite + react + shadcn + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app">
+      <h1>Home Inventory</h1>
+      <p>Vite + React + TypeScript is ready.</p>
+    </main>
+  );
+}
+
+export default App;
